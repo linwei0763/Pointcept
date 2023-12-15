c664a7fbfffe9ecf71fdf742565c18eb4087ea76
Wei Lin <102598371+linwei0763@users.noreply.github.com>
2023-12-15 14:40:39 +0000

    Update __init__.py

--- pointcept/models/__init__.py
+++ pointcept/models/__init__.py
@@ -6,7 +6,7 @@ from .sparse_unet import *
 from .point_transformer import *
 from .point_transformer_v2 import *
 
-from .stratified_transformer import *
+# from .stratified_transformer import *
 from .spvcnn import *
 from .octformer import *
 from .swin3d import *
